aa4575350712e64d6eeadb0f9a0514ddc3f548a3
Cynyard999 <CynyardQiu@outlook.com>
2020-05-26 12:33:24 +0800

    feat: 登陆界面自动跳转功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,40 +1,24 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
 Page({
   data: {
-    motto: '欢迎进入南跑跑',
+    motto: '欢迎来到南跑跑',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.switchTab({
-      url: '/pages/browse/browse'
-    })
-  },
-  intoApp: function(){
-    if(this.data.canIUse){
-      setTimeout(function () {
-        wx.switchTab({
-          url: '/pages/browse/browse',
-        })}
-        , 3000)
-    }
-    else{
-      console.log("NNNNNnn")
-      console.log(this.data.canIUse)
-    }
-    },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+      this.intoApp();
+    } 
+    else if (this.data.canIUse){
+      console.log("2"+app.globalData.userInfo)
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,8 +26,10 @@ Page({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
+        this.intoApp()
       }
-    } else {
+    } 
+    else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
@@ -57,14 +43,40 @@ Page({
     }
 },
   getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    if(e.detail.userInfo){
+      console.log(e)
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+      this.intoApp();
+    }
+    else{
+      wx.showModal({
+        title: '提示',
+        content: '请授权后再使用小程序',
+        showCancel: false,
+        confirmText: '确定',
+        success: function(res) {
+            if (res.confirm) {
+                console.log('用户拒绝了授权');
+            }
+        }
+      });
+    }
+  },
+  bindViewTap: function() {
+    wx.switchTab({
+      url: '/pages/browse/browse'
     })
+  },
+  intoApp: function(){
+    setTimeout(function () {
+      wx.switchTab({
+        url: '/pages/browse/browse',
+      })}
+      , 2000)
+    }
   }
-}
-
 );
-
