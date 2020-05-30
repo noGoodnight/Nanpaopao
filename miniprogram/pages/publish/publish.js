89393d7eceb92ab9088a298daa6c5f074d8e698c
Cynyard999 <CynyardQiu@outlook.com>
2020-05-30 15:39:12 +0800

    feat: 重写publish界面

--- miniprogram/pages/publish/publish.js
+++ miniprogram/pages/publish/publish.js
@@ -8,21 +8,94 @@ Page({
     minDate: new Date().getTime(),
     maxDate: new Date(new Date().getTime()+91*24*3600*1000).getTime(),
     currentDate: new Date().getTime(),
-    column:['其他','物品取送','事情帮办'],
-    show:false,
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`;
+      } 
+      else if (type === 'month') {
+        return `${value}月`;
+      } 
+      else if (type === 'day') {
+        return `${value}日`;
+      } 
+      else if (type === 'hour') {
+        return `${value}点`;
+      } 
+      else if (type === 'minute') {
+        return `${value}分`;    
+      }
+      return value;
+    },
+    showTimePicker:false,
+    start:"",
+    end:"",
+    amount:Number,
+    ddl:String,
+    description:String,
+    type:Number,
+    isFinished:0
+  },
+  placeChange(event) {
+    this.setData({
+
+    });
+  },
+  startClick(event) {
+    const { name } = event.currentTarget.dataset;
+    console.log("start click "+name)
+    this.setData({
+        start:name
+    });
+  },
+  endClick(event){
+    const { name } = event.currentTarget.dataset;
+    console.log("end click "+name)
+    this.setData({
+        end:name
+    });
+  },
+  typeClick(event){
+    const { name } = event.currentTarget.dataset;
+    this.setData({
+        type:name
+    });
+    console.log("type is "+this.data.type)
   },
   showPopup() {
-    this.setData({ show: true });
+    this.setData({ showTimePicker: true });
   },
-
-  onClose() {
-    this.setData({ show: false });
+  closePopup() {
+    this.setData({ showTimePicker: false });
   },
-  onInput(e) {
+  inputDDL(e) {
     this.setData({
       currentDate: e.detail,
     });
-  },
+    var date = new Date(e.detail);
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    if (month<10){
+      month = "0"+month;
+    }
+    var day = date.getDay();
+    if (day<10){
+      day = "0"+day;
+    }
+    var hour = date.getHours();
+    if (hour<10){
+      hour = "0"+hour;
+    }
+    var min = date.getMinutes();
+    if (min<10){
+      min = "0"+min;
+    }
+    this.data.ddl = year+"-"+month+"-"+day+" "+hour+":"+min
+    console.log(this.data.ddl)
+    this.closePopup();
+},
+
+
+
   /**
    * 生命周期函数--监听页面加载
    */
