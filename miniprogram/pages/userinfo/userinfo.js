cc844b49fe61e54d04e36ef36aa6685f89035ce3
Sjim <48637035+Sjim@users.noreply.github.com>
2020-06-16 09:54:39 +0800

    fix:修改了个人信息页面的刷新问题

--- miniprogram/pages/userinfo/userinfo.js
+++ miniprogram/pages/userinfo/userinfo.js
@@ -117,11 +117,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    this.onLoad()
     if (typeof this.getTabBar === 'function' && this.getTabBar()) {
         this.getTabBar().setData({
             active: 3
         });
     }
+    
   },
 
   /**
--- miniprogram/pages/userinfoSubpage/userInfoFile/userInfoFile.js
+++ miniprogram/pages/userinfoSubpage/userInfoFile/userInfoFile.js
@@ -170,10 +170,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({
-        active: 3
-      });
-    }
+    this.onLoad()
   },
 })
